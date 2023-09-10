71c6e71fd01b082ff7b473dae7e6bb4492713a6c
aerannakul <aerannnakul@gmail.com>
2023-09-10 18:42:33 +0530

    3_sum added

--- 15.3-sum.cpp
+++ 15.3-sum.cpp
@@ -5,11 +5,62 @@
  */
 
 // @lc code=start
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        
-    }
-};
-// @lc code=end
+#include <bits/stdc++.h>
+using namespace std;
 
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    for (int i = 0; i < nums.size() - 2; i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        int l = i + 1, r = nums.size() - 1;
+        while (l < r)
+        {
+            int sum = nums[i] + nums[l] + nums[r];
+            if (sum < 0)
+                l++;
+            else if (sum > 0)
+                r--;
+            else
+            {
+                ans.push_back({nums[i], nums[l], nums[r]});
+                while (l < r && nums[l] == nums[l + 1])
+                    l++;
+                while (l < r && nums[r] == nums[r - 1])
+                    r--;
+                l++;
+                r--;
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    vector<vector<int>> ans = threeSum(nums);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+
+    // @lc code=end
+
+    return 0;
+}
+// @lc code=end
